a0894268abb4de15fae244505a3556ec54c17996
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2024-02-15 18:58:55 +0530

    search api changes

--- src/components/SearchRestaurants.js
+++ src/components/SearchRestaurants.js
@@ -37,7 +37,7 @@ const SearchRestaurants = () => {
 
   const getSearchItemFoods = async () => {
     const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/dapi/landing/PRE_SEARCH?lat=12.9715987&lng=77.5945627"
+      "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Flanding%2FPRE_SEARCH%3Flat%3D12.9715987%26lng%3D77.5945627"
     );
     const json = await data.json();
     setFood(json?.data?.cards[1]?.card?.card?.imageGridCards?.info);
@@ -57,11 +57,13 @@ const SearchRestaurants = () => {
 
   const getSearchSuggestions = async () => {
     const data = await fetch(
-      `https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/search/suggest?lat=12.9715987&lng=77.5945627&str=${searchText}&trackingId=undefined`
+      "https://corsproxy.org/?" +
+        encodeURIComponent(
+          `https://www.swiggy.com/dapi/restaurants/search/suggest?lat=12.9715987&lng=77.5945627&str=${searchText}&trackingId=undefined`
+        )
     );
 
     const json = await data.json();
-    // console.log(json?.data?.suggestions)
     setSuggestions(json?.data?.suggestions);
   };
 
@@ -73,7 +75,10 @@ const SearchRestaurants = () => {
 
   const getSearchByRes = async () => {
     const data = await fetch(
-      `https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/search/v3?lat=12.9715987&lng=77.5945627&str=${searchName}&trackingId=undefined&submitAction=SUGGESTION&queryUniqueId=bc9bbc65-5d8b-0aa5-bbdc-65b467e6afdb`
+      "https://corsproxy.org/?" +
+        encodeURIComponent(
+          `https://www.swiggy.com/dapi/restaurants/search/v3?lat=12.9715987&lng=77.5945627&str=${searchName}&trackingId=undefined&submitAction=SUGGESTION&queryUniqueId=bc9bbc65-5d8b-0aa5-bbdc-65b467e6afdb`
+        )
     );
     const json = await data.json();
     if (isDish === "RESTAURANT") {
