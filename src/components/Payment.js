9f0c66a2334e331e765127ec15352696e46a5612
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2024-03-16 18:52:12 +0530

    razorpay integration

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -26,21 +26,78 @@ const Payment = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
-    axios
-      .post("api/payment", data)
-      .then((res) => {
-        console.log("res", res.json());
-
-        setTimeout(() => {
-          setLoading(false);
-        }, 1500);
-      })
-      .catch((error) => {
-        setLoading(false);
-        console.error(error);
+    const amount = 500;
+    const currency = "INR";
+    const receiptId ="Raz"+ Date.now();
+  
+  
+      const response = await fetch("https://razorpay-swiggy.onrender.com/order", {
+        method: "POST",
+        body: JSON.stringify({
+          amount,
+          currency,
+          receipt: receiptId,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
-  };
+      const order = await response.json();
+      console.log("order=>",order);
+  
+      var options = {
+        key: "rzp_test_13wQJzMWMgzywK", // Enter the Key ID generated from the Dashboard
+        amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        currency,
+        name: "HiTech Solutions", //your business name
+        description: "Test Transaction",
+        image: "https://example.com/your_logo",
+        order_id: order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+        handler: async function (response) {
+          const body = {
+            ...response,
+          };
+  
+          const validateRes = await fetch(
+            "https://razorpay-swiggy.onrender.com/order/validate",
+            {
+              method: "POST",
+              body: JSON.stringify(body),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          const jsonRes = await validateRes.json();
+          console.log("jsonRes",jsonRes);
+        },
+        prefill: {
+          //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+          name: "shubham Matrix", //your customer's name
+          email: "shubham@example.com",
+          contact: "9604567895", //Provide the customer's phone number for better conversion rates
+        },
+        notes: {
+          address: "Razorpay Corporate Office",
+        },
+        theme: {
+          color: "#3399cc",
+        },
+      };
+      var rzp1 = new window.Razorpay(options);
+      rzp1.on("payment.failed", function (response) {
+        alert(response.error.code);
+        alert(response.error.description);
+        alert(response.error.source);
+        alert(response.error.step);
+        alert(response.error.reason);
+        alert(response.error.metadata.order_id);
+        alert(response.error.metadata.payment_id);
+      });
+      rzp1.open();
+    };
+  
+  
   return (
     <div className="container mx-auto mt-8 max-w-md   bg-gradient-to-br from-yellow-300 via-green-400 to-blue-400 rounded p-4">
       <form
