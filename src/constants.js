5b179211f4a58b497b6599e7b57dc83174abd05b
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2025-02-28 16:31:34 +0530

    image cloudanary link changes

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,9 @@ export const Resturant_Data_URL =
 
 // https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
 export const IMG_CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  // "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
 
 export const FETCH_MENU_URL =
 // "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=43836";
